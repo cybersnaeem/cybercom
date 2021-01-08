8380f1b689a79e440b0b13b7c45ad12486599e00
cybersnaeem <naeemsvaliyani60660@gmail.com>
2021-01-08 17:35:44 +0530

    some change in programme 2

--- code/2021-01-08/code_ch_2.js
+++ code/2021-01-08/code_ch_2.js
@@ -1,8 +1,8 @@
-//define variables which contains the scores 
+
 var johnTeamScore = 88+120+103;
-var mikeTeamScore = 89+120+103;
-var maryTeamScore = 88+120+103;
-//97+134+105
+var mikeTeamScore = 77+120+103;
+var maryTeamScore = 97+134+105;
+//
 //average score of john
 var avgJohn = johnTeamScore/3;
 console.log('Average Score Of John : '+avgJohn);
@@ -12,44 +12,46 @@ var avgMike = mikeTeamScore/3;
 console.log('Average Score Of Mike : '+avgMike);
 
 
-//average score of Mary
+//average score of mary
 var avgMary = maryTeamScore/3;
 console.log('Average Score Of John : '+avgMary);
 
-//check that the score of both player is same or not
 if(avgJohn != avgMike){
-    //if the score of player is not same then compare avegare score..
-
-    if(avgJohn != avgMary){
-
-        if(avgMike != avgMary){
-            if(avgJohn > avgMike && avgJohn > avgMary){
-                //john wins
-                console.log('John\'s team win with '+ avgJohn +' score');
-            }
-            else if(avgMike > avgJohn && avgMike > avgMary){
-                //mike wins
-                console.log('Mark\'s team win with '+ avgMike +' score');
-            }
-            else{
-                //mary wins
-                console.log('Mary\'s team win with '+ avgMary +' score');
-            }
-        }
-        else{
-            //when the average score of mike and mary are same
-            console.log('!!!!....Match Draw Between Mike And Mary....!!!!');
-        }
-    }
+    if(avgJohn > avgMike ){
+        console.log('John\'s team win with '+ avgJohn +' score in front of Mike');
+    } 
     else{
-        //when the average score of john and mary are same
-        console.log('!!!!....Match Draw Between John And Mary....!!!!');
+        console.log('Mike\'s team win with '+ avgMike +' score in front of John');
     }
-
     
 }
-//when the average score of john and mike are same
 else{
     
     console.log('!!!!....Match Draw Between John And Mike....!!!!');
+}
+//john and mary
+if(avgJohn != avgMary){
+        if(avgJohn > avgMary){
+            console.log('John\'s team win with '+ avgJohn +' score in front of Mary');
+        }
+        else{
+            console.log('Mary\'s team win with '+ avgMary +' score in front of John');
+        }
+}
+else{
+    console.log('!!!!....Match Draw Between John And Mary....!!!!');
+}
+
+// mike and mary
+if(avgMike != avgMary){
+        if( avgMike  > avgMary){
+            
+            console.log('Mike\'s team win with '+ avgMike +' score in front of Mary');
+        }
+        else{
+            console.log('Mary\'s team win with '+ avgMary +' score in front of Mike');
+        }
+}
+else{
+    console.log('!!!!....Match Draw Between Mike And Mary....!!!!');
 }
