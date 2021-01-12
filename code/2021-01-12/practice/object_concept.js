c4f4d97ffda08bb2cfd4609fc3462b2d59d7ecdb
cybersnaeem <naeemsvaliyani60660@gmail.com>
2021-01-12 18:23:26 +0530

    add looping practice in this file

--- code/2021-01-12/practice/object_concept.js
+++ code/2021-01-12/practice/object_concept.js
@@ -17,3 +17,15 @@ myobj.lastName = 'Navadiya';
 myobj.isMarried = true
 
 console.log(myobj);
+
+//looping
+
+var myArr = ['naeem', 'dishant', 'rajesh', 'piyush', 177, 188, 'jk'];
+
+for (var i = 0; i <= myArr.length; i++) {
+    if (typeof(myArr[i]) !== 'string') {
+        continue;
+    } else {
+        console.log(myArr[i]);
+    }
+}
