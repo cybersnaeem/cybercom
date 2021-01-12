08333ef55e7d2d65554c5d5723fae82190fb6212
cybersnaeem <naeemsvaliyani60660@gmail.com>
2021-01-12 16:50:51 +0530

    practice done on object

---
+++ code/2021-01-12/practice/object_concept.js
@@ -0,0 +1,19 @@
+var naeemObj = {
+    firstName: 'naeem',
+    lastName: 'valiyani',
+    job: 'programmer',
+    salary: 22000,
+    isMarried: false,
+    hobby: ['cricket', 'programming', 'pubg']
+};
+
+console.log(naeemObj.lastName);
+console.log(naeemObj['hobby'][1])
+
+var myobj = new Object();
+
+myobj.firstName = 'Dishant';
+myobj.lastName = 'Navadiya';
+myobj.isMarried = true
+
+console.log(myobj);
