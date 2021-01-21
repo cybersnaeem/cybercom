e15fdd33091f585c4d6efd47e88b6a49afabdb27
cybersnaeem <19mcal090@charusat.edu.in>
2021-01-21 15:22:26 +0530

    display user data and update data half work

---
+++ code/test_2021-01-21/code/User.js
@@ -0,0 +1,96 @@
+var userArrayData = [];
+var userFormData = {};
+ 
+    if(localStorage.getItem('userData')){
+        userArrayData = JSON.parse(localStorage.getItem('userData'));
+    }
+//////////////////////////////////////////add user start-/////////////////////////
+ function addUser(){
+     
+     var name = document.getElementById('name').value;
+     var email = document.getElementById('email').value;
+     var password = document.getElementById('password').value;
+     var dateOfBirth =  document.getElementById('dob').value;
+     
+    userFormData = {
+       name:name,
+       email:email,
+       password:password,
+       dateOfBirth:dateOfBirth
+     };
+     console.log(userFormData);
+        
+     if(localStorage.getItem('userData')){
+        userArrayData.push(userFormData); 
+        localStorage.setItem('userData',JSON.stringify(userArrayData));
+       
+     }else{
+        userArrayData.push(userFormData); 
+        localStorage.setItem('userData',JSON.stringify(userArrayData));
+     } 
+ }
+ //////////////////////////////////////////end user data -/////////////////////////
+
+
+ //////////////////////////////////////////table data display start-/////////////////////////
+
+function getAge(date){
+    var d = new Date(date)
+    var dt = new Date();    
+    return dt.getFullYear() - d.getFullYear();
+}
+ 
+ var userData = JSON.parse(localStorage.getItem('userData'));
+ 
+var userTableData = `<table class="table table-bordered table-light" id="tableData">
+              <thead>
+                <tr>
+                  <th scope="col">Name</th>
+                  <th scope="col">Email</th>
+                  <th scope="col">Password</th>
+                  <th scope="col">Birthdate</th>
+                  <th scope="col">Age</th>
+                  <th scope="col" colspan="2">Actions</th>
+                </tr>
+              </thead>
+              <tbody>`
+               for(var i=0; i<userData.length ; i++){
+                 userTableData+= `<tr>
+                                <th scope="row">${userData[i].name}</th>
+                                <td>${userData[i].email}</td>
+                                <td>${userData[i].password}</td>
+                                <td>${userData[i].dateOfBirth}</td>
+                                <td>
+                                   ${getAge(userData[i].dateOfBirth)}
+                                </td>
+                                <td>
+                                    <button type="button" onclick="updateUser('${userData[i].name}','${userData[i].email}','${userData[i].password}','${userData[i].dateOfBirth}')" class="btn btn-link"> Update </button>
+                                </td>
+                                <td>
+                                <button type="button" onclick="deleteUser()" class="btn btn-link"> Delete </button>
+                                </td>
+                            </tr>`   
+               }
+              
+userTableData += `</tbody></table>`;
+
+document.getElementById('resultUser').innerHTML = userTableData ;
+//////////////////////////////////////////table data display finish-/////////////////////////
+
+
+
+function updateUser(name,email,pass,dob){
+    
+    document.getElementById('name').value= name;
+    document.getElementById('email').value= email;
+    document.getElementById('password').value= pass;
+    document.getElementById('dob').value= dob;
+    document.getElementById('addUser').value = 'Update User';
+
+    for(var i=0;i<userData.length;i++ ){
+        
+    }
+    //console.log(name,email,pass,dob);
+ }
+
+
