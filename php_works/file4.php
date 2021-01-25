8a5059e9e54727bc47160b22295c8ab84285672b
cybersnaeem <19mcal090@charusat.edu.in>
2021-01-25 15:17:28 +0530

    cancatination practice

---
+++ php_works/file4.php
@@ -0,0 +1,11 @@
+<?php
+    $day = 2;
+    $month = 'january';
+    $year = 2000;
+
+
+    //concat
+    echo "Current Date is <strong>$day,$month/$year</strong>";
+    echo "<br />";
+    echo "Current Date is".$day."//".$month."//".$year."</strong>";
+?>
