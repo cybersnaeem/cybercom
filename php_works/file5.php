19243c149736c9a460410b525b971e21b8232527
cybersnaeem <19mcal090@charusat.edu.in>
2021-01-25 15:20:17 +0530

    if else practice

---
+++ php_works/file5.php
@@ -0,0 +1,11 @@
+<?php
+    //if.. else practice
+    $name = "Naeem";
+    $age = 15;
+    if($age >= 18){
+        echo "$name is Eligible For Voting";
+    }
+    else{
+        echo "$name is Not Eligible For Voting";
+    }
+?>
